903eece3860a572d3bbacb8be2f9119fd637987c
Rendoru <maulana.firman56@gmail.com>
2019-05-20 14:38:58 +0700

    Done Sort

--- artifact/source/revised/ctfweb/myproblem.php
+++ artifact/source/revised/ctfweb/myproblem.php
@@ -4,19 +4,28 @@
 	include("auth.php");
 	include("db.php");
 	include("submitproblem.php");
+	$sort = "DESC";
+	$flipSort = "ASC";
+	if(isset($_GET['sort'])){
+		if($_GET['sort'] == "ASC"){
+			$sort = "ASC";
+			$flipSort = "DESC";
+		}
+	}
 ?>
 
 <?php include 'part/header.php'; ?>
 <?php include 'part/navbar.php'; ?>
 <h2 id='p1'>My Problems</h2>
+<a class="problem_sort" href="?sort=<?php echo $flipSort; ?>">SORT 	<img src="img/arrow.png" class="sort-<?php echo $flipSort; ?>"></a>
 <?php
 	$uname = $_SESSION['username'];
-	$queryp = "SELECT * FROM problems WHERE `username`='$uname'";
+	$queryp = "SELECT * FROM problems WHERE `username`='$uname' ORDER BY p_id ".$sort;
 	$resultp = mysqli_query($con,$queryp);
 	while($row = mysqli_fetch_assoc($resultp)){
 		?>
 		<div class='problemlist'>
-		
+
 		<div class='ptitle'><p style='text-align:left;'>
 			No. <?php echo $row['p_id']; ?>
 			- <?php echo $row['pname']; ?> <?php
