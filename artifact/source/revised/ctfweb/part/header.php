ea15e0fbfce39fe44ad374509b913add13349858
Rahma sofyantoro <rahma.sofyantoro16@mhs.if.its.ac.id>
2019-05-20 13:36:22 +0700

    Fix Conflict

--- artifact/source/revised/ctfweb/part/header.php
+++ artifact/source/revised/ctfweb/part/header.php
@@ -4,11 +4,8 @@
 <meta charset="utf-8">
 <title><?php echo $pageTitle; ?></title>
 <link rel="stylesheet" href="css/style.css">
-<<<<<<<
 <link rel="stylesheet" href="css/temporar.css">
-=======
 <link rel="stylesheet" href="css/all.min.css">
->>>>>>>
 <script src='http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>
 <script src='script/script.js'></script>
 </head>
