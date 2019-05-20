b3bcb54c250e101532925f348c371e1202e67d0a
Rendoru <maulana.firman56@gmail.com>
2019-05-20 21:03:22 +0700

    Stop tracking of DB

--- artifact/source/revised/ctfweb/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-//$con = mysqli_connect("localhost","root","","lolctf");
-$con = mysqli_connect("localhost","root","dadada","lolctf");
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-?>
